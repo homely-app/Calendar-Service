74b5e177dcb040e9109143ebded93f0ab1739b74
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2018-07-18 11:39:02 -0700

    Implement data generation MVP

--- generate.js
+++ generate.js
@@ -1,14 +1,68 @@
 var db = require('./models/');
+const faker = require('faker');
 
 class FakeDataGenerator {
   constructor() {
-    this.hello = 'world';
+    this.data = [];
   }
   createData() {
-    let data;
-    return data;
+    for (let i = 0; i < 100; i++) {
+      let dataItem = {};
+      dataItem.roomId = i;
+
+      dataItem.price = faker.random.number({
+        min: 50,
+        max: 400
+      });
+
+      dataItem.numberOfBookings = faker.random.number({
+        max: 5
+      });
+
+      dataItem.bookings = [];
+
+      // TODO - refine date generation algo to not produce overlapping bookings
+      for (let i = 0; i < dataItem.numberOfBookings; i++) {
+        let booking = {};
+        booking.checkIn = faker.date.between('2018-07-30', '2019-12-31');
+        booking.duration = faker.random.number({
+          min: 1,
+          max: 30
+        });
+        dataItem.bookings.push(booking.checkIn, booking.duration);
+      }
+
+      dataItem.serviceFee = faker.random.number({
+        min: 15,
+        max: 50
+      });
+
+      dataItem.cleaningFee = faker.random.number({
+        min: 5,
+        max: 35
+      });
+
+      dataItem.minimumStay = faker.random.number({
+        max: 3
+      });
+
+      this.data.push(dataItem);
+    }
+
+    // Shape of dataItem object
+    //   {
+    //     room_id: Number
+    //     numberOfBookings: Number
+    //     bookings:  Array [{checkIn: Date, duration: Number}]
+    //     price: Number
+    //     cleaningFee: Number
+    //     serviceFee: Number
+    //     minimumStay: Number
+    // }
+    return this.data;
   }
 }
 
 let myFactory = new FakeDataGenerator();
 let fakeData = myFactory.createData();
+console.log(fakeData);
--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const dbURI = process.env.MONGODB_URI || "mongodb://localhost/chess-pro"
+const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/bookings';
 
 mongoose.connect(dbURI);
 
@@ -9,7 +9,7 @@ mongoose.connection.on('connected', function() {
 });
 
 // If the connection throws an error
-mongoose.connection.on('error', function(err){
+mongoose.connection.on('error', function(err) {
   console.log('Mongoose default connection error: ' + err);
 });
 
@@ -20,8 +20,10 @@ mongoose.connection.on('disconnected', function() {
 
 // If the Node process ends, close the Mongoose connection
 process.on('SIGINT', function() {
-  mongoose.connection.close( function() {
-    console.log('Mongoose default connection disconnected through app termination');
+  mongoose.connection.close(function() {
+    console.log(
+      'Mongoose default connection disconnected through app termination'
+    );
     process.exit(0);
   });
 });
