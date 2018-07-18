8a894ed7dbfe4e80dec483b7716ffaacf0440c22
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2018-07-18 10:34:00 -0700

    Initialize FakeDataGenerator and install faker

--- generate.js
+++ generate.js
@@ -1,1 +1,14 @@
 var db = require('./models/');
+
+class FakeDataGenerator {
+  constructor() {
+    this.hello = 'world';
+  }
+  createData() {
+    let data;
+    return data;
+  }
+}
+
+let myFactory = new FakeDataGenerator();
+let fakeData = myFactory.createData();
