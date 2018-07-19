e04d2f0e6d999dc6e1373d3b1d789862d3e947a1
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2018-07-19 09:44:07 -0700

    Implement getData

--- client/src/components/Applet/Applet.js
+++ client/src/components/Applet/Applet.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import FontAwesome from 'react-fontawesome';
 
+const domain = process.env.DOMAIN || 'https://localhost:';
+const port = process.env.PORT || '1128';
+
 class Applet extends Component {
   constructor(props) {
     super(props);
@@ -9,14 +12,17 @@ class Applet extends Component {
     };
   }
 
-  // componentWillMount(){}
-  // componentDidMount(){}
-  // componentWillUnmount(){}
+  getData() {
+    let data = JSON.stringify({ data: 'hello world' });
+    const endpoint = '/repos';
+    this.postData(domain + port + endpoint, data)
+      .then(data => console.log(data)) // JSON from `response.json()` call
+      .catch(error => console.error(error));
+  }
 
-  // componentWillReceiveProps(){}
-  // shouldComponentUpdate(){}
-  // componentWillUpdate(){}
-  // componentDidUpdate(){}
+  componentDidMount() {
+    this.getData();
+  }
 
   render() {
     return (
