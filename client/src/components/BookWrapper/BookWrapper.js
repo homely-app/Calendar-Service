1ab3ba2fac522457f1e973e3ef846b5cc7e6d3ed
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2018-07-20 10:53:42 -0700

    Implement dynamic price rendering

--- client/src/components/BookWrapper/BookWrapper.js
+++ client/src/components/BookWrapper/BookWrapper.js
@@ -7,30 +7,34 @@ class BookWrapper extends Component {
 
   render() {
     return (
-      <div className="container">
-        <div className="price-container">
-          <h2 className="price">
-            {' '}
-            $69 <span className="price-pernight">per night</span>
-          </h2>
-        </div>
-        <div className="book-container">
-          <h3 className="sub-title">Dates</h3>
-          <div className="book-subcontainer">
-            <div className="book-checkin-container">
-              <h3 className="book-checkin">Check-in</h3>
+      <div>
+        {this.props.roomData ? (
+          <div className="container">
+            <div className="price-container">
+              <h2 className="price">
+                ${this.props.roomData.price}
+                <span className="price-pernight"> per night</span>
+              </h2>
             </div>
-            <div className="book-checkout-container">
-              <h3 className="book-checkout">Check-out</h3>
+            <div className="book-container">
+              <h3 className="sub-title">Dates</h3>
+              <div className="book-subcontainer">
+                <div className="book-checkin-container">
+                  <h3 className="book-checkin">Check-in</h3>
+                </div>
+                <div className="book-checkout-container">
+                  <h3 className="book-checkout">Check-out</h3>
+                </div>
+              </div>
+              <h3 className="sub-title">Guests</h3>
+              <div className="guest-subcontainer">
+                <h3> 3 Guest</h3>
+              </div>
+              <button id="book-button">Book</button>
+              <p className="disclaimer">You won’t be charged yet</p>
             </div>
           </div>
-          <h3 className="sub-title">Guests</h3>
-          <div className="guest-subcontainer">
-            <h3> 3 Guest</h3>
-          </div>
-          <button id="book-button">Book</button>
-          <p className="disclaimer">You won’t be charged yet</p>
-        </div>
+        ) : null}
       </div>
     );
   }
