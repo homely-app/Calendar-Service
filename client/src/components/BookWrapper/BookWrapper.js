4b11cc14a3e85cd732f1c9e1447288e4252ab38e
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2018-07-20 14:55:04 -0700

    Componentize pricingWrapper

--- client/src/components/BookWrapper/BookWrapper.js
+++ client/src/components/BookWrapper/BookWrapper.js
@@ -1,22 +1,12 @@
 import React, { Component } from 'react';
 import FontAwesome from 'react-fontawesome';
-
+import PricingWrapper from '../PricingWrapper';
 class BookWrapper extends Component {
   constructor(props) {
     super(props);
   }
 
   render() {
-    // TODO: correct this function
-    let totalBookingPrice;
-    if (this.props.roomData) {
-      totalBookingPrice =
-        this.props.roomData.price +
-        this.props.roomData.serviceFee +
-        this.props.roomData.cleaningFee +
-        this.props.roomData.taxes;
-    }
-
     return (
       <React.Fragment>
         {this.props.roomData ? (
@@ -70,34 +60,7 @@ class BookWrapper extends Component {
                   1 Guest
                 </h3>
               </div>
-              <div className="pricing-container">
-                <div className="pricing-subcontainer">
-                  <p className="pricing-item">
-                    ${this.props.roomData.price} x 1 night
-                  </p>
-                  <p className="pricing-item">${this.props.roomData.price}</p>
-                </div>
-                <div className="pricing-subcontainer">
-                  <p className="pricing-item"> Service fee</p>
-                  <p className="pricing-item">
-                    ${this.props.roomData.serviceFee}
-                  </p>
-                </div>
-                <div className="pricing-subcontainer">
-                  <p className="pricing-item">Cleaning fee</p>
-                  <p className="pricing-item">
-                    ${this.props.roomData.cleaningFee}
-                  </p>
-                </div>
-                <div className="pricing-subcontainer">
-                  <p className="pricing-item"> Occupancy taxes and fees</p>
-                  <p className="pricing-item">${this.props.roomData.taxes}</p>
-                </div>
-                <div className="pricing-subcontainer">
-                  <p className="pricing-total"> Total</p>
-                  <p className="pricing-total">${totalBookingPrice}</p>
-                </div>
-              </div>
+              <PricingWrapper roomData={this.props.roomData} />
               <button id="book-button" onClick={e => this.props.handleClick(e)}>
                 Book
               </button>
