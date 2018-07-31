812c54d064c3687cc1263395a919ee30542a0620
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2018-07-31 16:52:02 -0700

    Add grunt task runner and deploy script

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,29 @@
-grunt.initConfig({
-  aws: grunt.file.readJSON("aws-keys.json"),
-
-  aws_s3: {
-    options: {
-      accessKeyId: "<%= aws.AWSAccessKeyId %>",
-      secretAccessKey: "<%= aws.AWSSecretKey %>"
-    },
-    dist: {
+module.exports = function(grunt) {
+  grunt.initConfig({
+    aws: grunt.file.readJSON("aws-keys.json"),
+    aws_s3: {
       options: {
-        bucket: "airfec2018"
+        accessKeyId: "<%= aws.AWSAccessKeyId %>",
+        secretAccessKey: "<%= aws.AWSSecretKey %>",
+        region: "us-west-1"
+      },
+      dist: {
+        options: {
+          bucket: "airfec2018"
+        },
+        files: [
+          {
+            expand: true,
+            cwd: "client/dist/",
+            src: ["**"],
+            dest: "/scripts"
+          }
+        ]
       }
     }
-  }
-});
+  });
+
+  grunt.loadNpmTasks("grunt-aws-s3");
+
+  grunt.registerTask("deploy", "aws_s3");
+};
