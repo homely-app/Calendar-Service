e20b732a9ff9e61b7953204b1fe26eab18061d38
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2018-08-01 17:37:21 -0700

    Implement docker

--- models/index.js
+++ models/index.js
@@ -1,31 +1,36 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const IP = "database:27017";
+const dbURI = process.env.MONGODB_URI || `mongodb://${IP}/bookings`;
 
-const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/bookings';
+mongoose.connect(
+  dbURI,
+  {
+    useNewUrlParser: true
+  }
+);
 
-mongoose.connect(dbURI);
-
-mongoose.connection.on('connected', function() {
-  console.log('Mongoose default connection open to ' + dbURI);
+mongoose.connection.on("connected", function() {
+  console.log("Mongoose default connection open to " + dbURI);
 });
 
 // If the connection throws an error
-mongoose.connection.on('error', function(err) {
-  console.log('Mongoose default connection error: ' + err);
+mongoose.connection.on("error", function(err) {
+  console.log("Mongoose default connection error: " + err);
 });
 
 // When the connection is disconnected
-mongoose.connection.on('disconnected', function() {
-  console.log('Mongoose default connection disconnected');
+mongoose.connection.on("disconnected", function() {
+  console.log("Mongoose default connection disconnected");
 });
 
 // If the Node process ends, close the Mongoose connection
-process.on('SIGINT', function() {
+process.on("SIGINT", function() {
   mongoose.connection.close(function() {
     console.log(
-      'Mongoose default connection disconnected through app termination'
+      "Mongoose default connection disconnected through app termination"
     );
     process.exit(0);
   });
 });
 
-module.exports = { Booking: require('./Booking') };
+module.exports = { Booking: require("./Booking") };
