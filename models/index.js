d3d5be9c4224d75855239f3a2bfa95edd7f88325
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2018-08-03 11:26:09 -0700

    Implement responsive header design and add favicon

--- models/index.js
+++ models/index.js
@@ -1,5 +1,7 @@
-const mongoose = require("mongoose");
-const IP = "database:27017";
+const mongoose = require('mongoose');
+
+//TODO: change localhost to database
+const IP = 'localhost:27017';
 const dbURI = process.env.MONGODB_URI || `mongodb://${IP}/bookings`;
 
 mongoose.connect(
@@ -9,28 +11,28 @@ mongoose.connect(
   }
 );
 
-mongoose.connection.on("connected", function() {
-  console.log("Mongoose default connection open to " + dbURI);
+mongoose.connection.on('connected', function() {
+  console.log('Mongoose default connection open to ' + dbURI);
 });
 
 // If the connection throws an error
-mongoose.connection.on("error", function(err) {
-  console.log("Mongoose default connection error: " + err);
+mongoose.connection.on('error', function(err) {
+  console.log('Mongoose default connection error: ' + err);
 });
 
 // When the connection is disconnected
-mongoose.connection.on("disconnected", function() {
-  console.log("Mongoose default connection disconnected");
+mongoose.connection.on('disconnected', function() {
+  console.log('Mongoose default connection disconnected');
 });
 
 // If the Node process ends, close the Mongoose connection
-process.on("SIGINT", function() {
+process.on('SIGINT', function() {
   mongoose.connection.close(function() {
     console.log(
-      "Mongoose default connection disconnected through app termination"
+      'Mongoose default connection disconnected through app termination'
     );
     process.exit(0);
   });
 });
 
-module.exports = { Booking: require("./Booking") };
+module.exports = { Booking: require('./Booking') };
