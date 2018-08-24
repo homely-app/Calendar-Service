b79b5d244388b4344b07726d4673c4c9fe7a4837
courtchao <couchao@ucdavis.edu>
2018-08-23 21:11:02 -0700

    Modify routes to direct to calendar

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,16 @@ const express = require("express");
 const ctrl = require("./../controllers");
 const router = express.Router();
 
-// Get info for bookings
-router.get("/rooms/:id/bookings", ctrl.bookings.get);
+// Get info for calendar
+router.get("/rooms/:id/calendar", ctrl.calendar.getRoom);
 
-// Put info for bookings
-router.put("/rooms/:id/bookings", ctrl.bookings.put);
+// Put info for calendar
+//router.put("/rooms/:id/calendar", ctrl.calendar.putRoom);
 
-// Post info for bookings
-router.post("/rooms/:id/bookings", ctrl.bookings.post);
+// Post info for calendar
+// router.post("/rooms/:id/calendar", ctrl.calendar.postRoom);
 
-// Delete info for bookings
-router.delete("/rooms/:id/bookings", ctrl.bookings.delete);
+// Delete info for calendar
+// router.delete("/rooms/:id/calendar", ctrl.calendar.deleteRoom);
 
 module.exports = router;
