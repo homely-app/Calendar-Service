1ed01bd2d7efc7ed0c538ecfb22a05a91a218543
courtchao <couchao@ucdavis.edu>
2018-08-23 21:08:07 -0700

    Add siege file

---
+++ siege.js
@@ -0,0 +1,19 @@
+const siege = require('siege');
+
+const randomNumbers = [];
+
+const random = (min, max) => Math.floor(Math.random() * max) + min;
+
+const arrSize = 450000;
+
+for (let i = 0; i < arrSize; i += 1) {
+  randomNumbers.push(random(9500000, 10000000));
+}
+
+let sieger = siege().on(3001).concurrent(6);
+
+for (let i = 0; i < randomNumbers.length; i += 1) {
+  sieger = sieger.for(1).times.get(`/api/rooms/${randomNumbers[i]}/calendar`);
+}
+
+sieger.attack();
