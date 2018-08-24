ea6d093d0f9512557cd707ddc71fb149f4440b88
courtchao <couchao@ucdavis.edu>
2018-08-23 21:09:25 -0700

    Change server port

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
+const newRelic = require('newrelic');
 const express = require('express');
 const routes = require('./../routes');
 const path = require('path');
+const ctrl = require('../controllers');
 
 const app = express();
 
-app.set('port', process.env.PORT || 3001);
+app.set('port', process.env.PORT || 80);
 
 app.get('/', function(req, res) {
   res.redirect('/rooms/1');
@@ -13,25 +15,31 @@ app.get('/', function(req, res) {
 app.use(express.static('public/'));
 app.use(express.static('client/dist'));
 
-app.get('/rooms/:id', function(req, res) {
+// app.get('/rooms/:id', function(req, res) {
+//   const reactPath = path.join(__dirname, '../public/index.html');
+//   res.sendFile(reactPath);
+// });
+
+app.get('/rooms/:id', (req, res) => {
+  console.log('inside get on server');
   const reactPath = path.join(__dirname, '../public/index.html');
   res.sendFile(reactPath);
 });
 
-app.put('/rooms/:id', function(req, res) {
-  const reactPath = path.join(__dirname, '../public/index.html');
-  res.sendFile(reactPath);
-});
+// app.put('/rooms/:id', function(req, res) {
+//   const reactPath = path.join(__dirname, '../public/index.html');
+//   res.sendFile(reactPath);
+// });
 
-app.post('/rooms/:id', function(req, res) {
-  const reactPath = path.join(__dirname, '../public/index.html');
-  res.sendFile(reactPath);
-});
+// app.post('/rooms/:id', function(req, res) {
+//   const reactPath = path.join(__dirname, '../public/index.html');
+//   res.sendFile(reactPath);
+// });
 
-app.delete('/rooms/:id', function(req, res) {
-  const reactPath = path.join(__dirname, '../public/index.html');
-  res.sendFile(reactPath);
-});
+// app.delete('/rooms/:id', function(req, res) {
+//   const reactPath = path.join(__dirname, '../public/index.html');
+//   res.sendFile(reactPath);
+// });
 
 app.use('/api', routes);
 
